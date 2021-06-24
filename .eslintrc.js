a80bbfd8891c8a4a6e5dedcb2fd84f0603e41b85
pedro <pedro@thinkanddev.com>
2021-06-24 15:16:28 -0300

    Add eslint and pretier, move ABIS

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:vue/essential',
+    'plugin:prettier/recommended',
+    'prettier/vue',
+  ],
+  plugins: [
+    'prettier'
+  ],
+  parserOptions: {
+    parser: 'babel-eslint',
+  },
+  rules: {
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-warning-comments': 'warn',
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'prettier/prettier': 'error',
+    'no-console': [
+      'warn',
+      {
+        'allow': ['warn', 'error']
+      }
+    ],
+  },
+};
