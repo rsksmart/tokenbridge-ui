7660136ed91d170fb506489665d84d0aa2ac1a74
Jonathan Huamani <jonathan.huamani@iovlabs.org>
2021-08-27 13:31:49 -0500

    feat: added deprecated to TXN_Storage class feat: added toLowerCase on `createStoreKey` method feat: moved database default name to an independent variable
    Signed-off-by: Jonathan Huamani <jonathan.huamani@iovlabs.org>

--- src/config/offlineDB.connection.js
+++ src/config/offlineDB.connection.js
@@ -1,6 +1,8 @@
 import Dexie from 'dexie'
 
-const dbInstance = new Dexie(process.env.VUE_APP_OFFLINE_DB || 'tknbrgoff')
+const DB_DEFAULT_NAME = 'tknbrgoff'
+
+const dbInstance = new Dexie(process.env.VUE_APP_OFFLINE_DB || DB_DEFAULT_NAME)
 dbInstance.version(2).stores({
   transactions: `transactionHash, accountAddress, currentStep, type, networkId, tokenFrom, tokenTo, amount, receiveAmount, senderAddress, receiverAddress, timestamp, transactionIndex, blockHash, blockNumber, cumulativeGasUsed, gasUsed, contractAddress, logs, from, to, status, logsBloom, effectiveGasPrice`,
 })
