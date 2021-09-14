bb9ddc8bc9f43f77eb0ab0f3586fe284a0ded457
Jonathan Huamani <jonathan.huamani@iovlabs.org>
2021-09-14 16:15:25 -0500

    feat: Added approve promise to resolve call
    Signed-off-by: Jonathan Huamani <jonathan.huamani@iovlabs.org>

--- src/utils/text-helpers.js
+++ src/utils/text-helpers.js
@@ -1,1 +1,11 @@
+import { sanitizeTxHash } from '@/utils/index'
+
 export const isEmptyAddress = addressString => /(0x)[0]+/i.test(addressString)
+
+export const txExplorerLink = (txHash, explorerLink) => {
+  const sanitizedTxHash = sanitizeTxHash(txHash)
+  if (!txHash) {
+    return ''
+  }
+  return `<a target="_blank" href="${explorerLink}/tx/${sanitizedTxHash}">see Tx</a>`
+}
