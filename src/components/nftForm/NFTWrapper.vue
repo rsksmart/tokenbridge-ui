c152af75ab3c505200ea313a68fd05c1e51930d8
Jonathan Huamani <jonathan.huamani@iovlabs.org>
2021-09-14 16:07:02 -0500

    feat: Added nft cross button feat: Enable logic for approve nft
    Signed-off-by: Jonathan Huamani <jonathan.huamani@iovlabs.org>

--- src/components/nftForm/NFTWrapper.vue
+++ src/components/nftForm/NFTWrapper.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="nft-wrapper container p-5 my-5">
-    <div class="row">
-      <div class="col-md-12">
-        <NFTForm :origin-network="originNetwork" @onSuccess="handleOnSuccess" />
+  <div>
+    <div class="nft-wrapper container p-5 mt-5">
+      <div class="row">
+        <div class="col-md-12 p-2">
+          <NFTForm :origin-network="originNetwork" @onSuccess="handleOnSuccess" />
+        </div>
+        <div v-if="displayInformation" class="col-md-12 p-2">
+          <NFTViewInformation :metadata="metadata"></NFTViewInformation>
+        </div>
+        <div v-if="displayInformation" class="col-md-12 p-2">
+          <NFTDestinationForm
+            ref="destinationForm"
+            :destination-network="destinationNetwork"
+            :web3-contract="web3Contract"
+            :nft-contract-address="nftContractAddress"
+            :token-id="tokenId"
+          ></NFTDestinationForm>
+        </div>
       </div>
-      <div v-if="displayInformation" class="col-md-12 p-2">
-        <NFTViewInformation
-          :destination-network="destinationNetwork"
-          :metadata="metadata"
-        ></NFTViewInformation>
+    </div>
+    <div class="p-5">
+      <div class="row justify-content-center">
+        <button
+          v-if="!isApproved"
+          class="btn btn-primary"
+          :disabled="!loadedInfo"
+          @click="handleApprove"
+        >
+          Approve
+        </button>
+        <button class="btn btn-primary" :disabled="!loadedInfo" @click="handleCrossNFT">
+          Cross NFT
+        </button>
       </div>
     </div>
   </div>
@@ -18,17 +41,34 @@
 import NFTViewInformation from '@/components/nftForm/NFTViewInformation'
 import NFTForm from '@/components/nftForm/NFTForm'
 import { store } from '@/store'
+import NFTDestinationForm from '@/components/nftForm/NFTDestinationForm'
+import SIDE_NFT_TOKEN from '@/constants/abis/side_nft_token.json'
+import { waitForReceipt } from "@/utils";
 
 export default {
   name: 'NFTWrapper',
-  components: { NFTViewInformation, NFTForm },
+  components: { NFTViewInformation, NFTForm, NFTDestinationForm },
+  props: {
+    rskFee: {
+      type: Number,
+      required: true,
+    },
+    ethFee: {
+      type: Number,
+      required: true,
+    },
+  },
   data() {
     return {
+      loadedInfo: false,
       nftAddress: '',
       tokenId: '',
       sharedState: store.state,
       metadata: {},
       displayInformation: false,
+      web3Contract: null,
+      nftContractAddress: '',
+      isApproved: false,
     }
   },
   computed: {
@@ -40,9 +80,53 @@ export default {
     },
   },
   methods: {
-    handleOnSuccess(metadata) {
+    handleOnSuccess({ metadata, web3Contract, nftContractAddress, tokenId, isApproved }) {
       this.metadata = metadata
       this.displayInformation = true
+      this.web3Contract = web3Contract
+      this.nftContractAddress = nftContractAddress
+      this.tokenId = tokenId
+      this.isApproved = isApproved
+      this.loadedInfo = true
+    },
+    async handleCrossNFT() {
+      await this.$refs.destinationForm.handleSubmitExternal()
+    },
+    async handleApprove() {
+      // TODO: Check Correct address
+      const bridgeAddress = this.sharedState.currentConfig.bridge
+      const web3 = this.sharedState.web3
+      const accountAddress = this.sharedState.accountAddress
+      try {
+        const tokenContract = new web3.eth.Contract(SIDE_NFT_TOKEN, this.nftContractAddress)
+        const gasPrice = await store.getGasPriceHex()
+
+        return new Promise((resolve, reject) => {
+          tokenContract.methods
+            .setApprovalForAll(bridgeAddress, false)
+            .send({ from: accountAddress, gasPrice, gas: 70_000 }, async (err, txHash) => {
+              const txExplorerLink = this.txExplorerLink(txHash)
+              if (err) {
+                throw new Error(`Execution failed ${err.message} ${txExplorerLink}`)
+              }
+              try {
+                const receipt = await waitForReceipt(txHash, web3)
+                if (receipt.status) {
+                  return resolve(receipt)
+                } else {
+                  return reject(
+                    new Error(`Transaction status failed ${err.message} ${txExplorerLink}`),
+                  )
+                }
+              } catch (error) {
+                return reject(new Error(`${error} ${txExplorerLink}`))
+              }
+            })
+        })
+
+      } catch (approvalError) {
+        console.error('Approve failed: ' + JSON.stringify(approvalError))
+      }
     },
   },
 }
---
+++ src/validators/transactions.js
@@ -0,0 +1,9 @@
+export const rules = {
+  required: value => {
+    if (!value || !value.length) {
+      return 'This field is required'
+    }
+
+    return true
+  },
+}
