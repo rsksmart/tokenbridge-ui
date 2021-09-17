899afe8c04bd55e745a5f9c54b304888177b6d46
Jonathan Huamani <jonathan.huamani@iovlabs.org>
2021-09-16 23:03:53 -0500

    feat: Added modal dialog to execute when is error or successfull the operations fix: Fixed error on network settings feat: Added support for NFT
    Signed-off-by: Jonathan Huamani <jonathan.huamani@iovlabs.org>

--- src/App.vue
+++ src/App.vue
@@ -3,19 +3,28 @@
     <NavBar />
     <router-view />
     <Footer />
+    <WrapperModal ref="wrapperModal" />
   </div>
 </template>
 
 <script>
+import { provide, ref } from 'vue'
 // import Connection from '@/components/commons/Connection.vue'
 import NavBar from '@/components/layouts/NavBar.vue'
 import Footer from '@/components/layouts/Footer.vue'
+import WrapperModal from '@/components/commons/Modals/WrapperModal'
 
 export default {
   name: 'App',
   components: {
     NavBar,
     Footer,
+    WrapperModal,
+  },
+  setup() {
+    const wrapperModal = ref(null)
+    provide('$modal', wrapperModal)
+    return { wrapperModal }
   },
 }
 </script>
---
+++ src/components/commons/Modals/SuccessModal.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="bg-white p-2 rounded-sm">
+    <div class="modal-header">
+      <h4 class="m-0"><i class="fas fa-check-circle text-success"></i> {{ title }}</h4>
+    </div>
+    <div class="modal-body">
+      <p v-html="message"></p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SuccessModal',
+  props: {
+    title: { type: String, default: 'Successful' },
+    message: { type: String, required: true },
+  },
+}
+</script>
+
+<style scoped></style>
