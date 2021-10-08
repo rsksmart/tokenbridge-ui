d1c8a379b338b1f2a398e1bc784aa9fc6ffc72d7
Jonathan Huamani <jonathan.huamani@iovlabs.org>
2021-10-07 21:50:31 -0500

    feat: Added description to DB schema
    Signed-off-by: Jonathan Huamani <jonathan.huamani@iovlabs.org>

--- src/config/offlineDB.connection.js
+++ src/config/offlineDB.connection.js
@@ -3,6 +3,21 @@ import Dexie from 'dexie'
 const DB_DEFAULT_NAME = 'tknbrgoff'
 
 const dbInstance = new Dexie(process.env.VUE_APP_OFFLINE_DB || DB_DEFAULT_NAME)
+/**
+ * indexedDB Schema:
+ *  - transactionHash: primary key <string>
+ *  - accountsAddresses: Array<string>
+ *  - networkId: number
+ *  - timestamp: number
+ *  - blockNumber: number
+ *  - receiveAmount: float "It's used only for ERC20 transactions"
+ *  - currentStep: number
+ *  - type: string "the type of transaction, by default is 'Cross'"
+ *  - senderAddress: string
+ *  - receiverAddress: string
+ *  - tokenId: number "Added on v4 to support ERC721 transactions"
+ *  - tokenType: string "Added on v4 to check the kind of transaction"
+ */
 dbInstance.version(4).stores({
   transactions: `transactionHash, *accountsAddresses, networkId, timestamp, blockNumber, receiveAmount, currentStep, type, senderAddress, receiverAddress, tokenId, tokenType`,
 })
