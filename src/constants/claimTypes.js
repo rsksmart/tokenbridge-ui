4e8980a6d3e9a51429f6a650f8bf161bc3772d5e
Jonathan Huamani <jonathan.huamani@iovlabs.org>
2021-12-02 09:42:13 -0500

    feat: sign metamask message on claim button in modal

---
+++ src/constants/claimTypes.js
@@ -0,0 +1,9 @@
+export const STANDARD = 'standard'
+export const PAY_WITH_TOKENS = 'payWithTokens'
+export const CONVERT_TO_RBTC = 'convertToRBTC'
+
+export const CLAIM_TYPES = {
+  STANDARD,
+  PAY_WITH_TOKENS,
+  CONVERT_TO_RBTC,
+}
