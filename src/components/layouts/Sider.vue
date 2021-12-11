2c71df29852a3306ae96d5ccb24234c3da690e33
Jonathan Huamani <jonathan.huamani@iovlabs.org>
2021-12-10 19:03:06 -0500

    feat: Added sider Component to contain menu and logo in left side
    Signed-off-by: Jonathan Huamani <jonathan.huamani@iovlabs.org>

---
+++ src/components/layouts/Sider.vue
@@ -0,0 +1,45 @@
+<template>
+  <aside class="sider">
+    <div class="h-100">
+      <div class="sider-logo">
+        <a class="navbar-brand" href="https://rsk.co">
+          <img
+            id="logo"
+            src="../../assets/img/bridge-logo.png"
+            class="logo"
+            alt="RSK Token Bridge"
+          />
+        </a>
+      </div>
+      <Menu />
+    </div>
+    <div class="fixed-bottom w-100 m-4">
+      <a id="help" class="text-primary" href="https://developers.rsk.co/slack/">
+        Do you need help?
+      </a>
+    </div>
+  </aside>
+</template>
+
+<script>
+import Menu from '@/components/layouts/Menu'
+
+export default {
+  name: 'Sider',
+  components: { Menu },
+}
+</script>
+
+<style scoped>
+.sider {
+  width: 200px;
+  background-color: #ecf0f1;
+  height: 100vh;
+  position: sticky;
+  flex: 0 0 200px;
+  top: 0;
+}
+.sider-logo {
+  text-align: center;
+}
+</style>
