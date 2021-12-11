e1141b1bcaa95d83fc4678c83e1bb410e04c7644
Jonathan Huamani <jonathan.huamani@iovlabs.org>
2021-12-10 19:04:30 -0500

    feat: Added sider component to App component
    Signed-off-by: Jonathan Huamani <jonathan.huamani@iovlabs.org>

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,13 @@
 <template>
   <div id="main">
-    <NavBar />
-    <router-view />
-    <Footer />
+    <section class="d-flex">
+      <Sider />
+      <section class="d-flex flex-column w-100 justify-content-between">
+        <NavBar />
+        <router-view />
+        <Footer />
+      </section>
+    </section>
     <WrapperModal ref="wrapperModal" />
   </div>
 </template>
@@ -12,6 +17,7 @@ import { provide, ref } from 'vue'
 // import Connection from '@/components/commons/Connection.vue'
 import NavBar from '@/components/layouts/NavBar.vue'
 import Footer from '@/components/layouts/Footer.vue'
+import Sider from '@/components/layouts/Sider'
 import WrapperModal from '@/components/commons/Modals/WrapperModal'
 
 export default {
@@ -20,6 +26,7 @@ export default {
     NavBar,
     Footer,
     WrapperModal,
+    Sider,
   },
   setup() {
     const wrapperModal = ref(null)
