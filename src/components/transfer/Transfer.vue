dc57af2594202848a7e43fcedb9b59682327ef5d
Jonathan Huamani <jonathan.huamani@iovlabs.org>
2021-12-13 23:02:04 -0500

    feat: transfer component

---
+++ src/components/transfer/Transfer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>{{ title }}</div>
+</template>
+
+<script>
+export default {
+  name: 'Transfer',
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
+<style scoped></style>
