61c06562214498da9e78b1acc8bf5e4172294d1c
aguschirico <aguschirico@gmail.com>
2022-02-22 17:11:05 +0100

    partial. Styles got broken.

--- src/main.js
+++ src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import { defineRule } from 'vee-validate'
 import App from './App.vue'
+import vuetify from './plugins/vuetify'
 import router from './router'
 
+import { loadFonts } from './plugins/webfontloader'
 import { rules } from './validators/transactions'
-import './styles/main.css'
+import '@/styles/main.css'
 import ServicesPlugin from '@/plugins/ServicesPlugin'
 import { TransactionService } from '@/modules/transactions/transactions.service'
 
@@ -12,7 +14,10 @@ Object.keys(rules).forEach(rule => {
   defineRule(rule, rules[rule])
 })
 
+loadFonts()
+
 createApp(App)
   .use(router)
   .use(ServicesPlugin, { TransactionService })
+  .use(vuetify)
   .mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,49 @@
 const { version } = require('./package.json')
+const webpack = require('webpack');
 
 module.exports = {
-  lintOnSave: false,
+  lintOnSave: true,
+  chainWebpack: config => {
+    const types = ['vue-modules', 'vue', 'normal-modules', 'normal']
+    types.forEach(type => addStyleResource(config.module.rule('stylus').oneOf(type)))
+  },
   configureWebpack: {
+    resolve: {
+      fallback: { 
+        "os": require.resolve("os-browserify/browser"),
+        "https": require.resolve("https-browserify"),
+        "http": require.resolve("stream-http"),
+        "crypto": require.resolve("crypto-browserify"),
+        "stream": require.resolve("stream-browserify"),
+        "buffer": require.resolve("buffer")
+      }
+    },
+    plugins: [
+      new webpack.ProvidePlugin({
+        Buffer: ['buffer', 'Buffer'],
+      }),
+      new webpack.ProvidePlugin({
+        process: 'process/browser',
+      }),
+    ],
     output: {
       filename: `[name]-${version}.[hash].bundle.js`,
     },
   },
+
+  pluginOptions: {
+    vuetify: {
+      // https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
+    },
+  },
+}
+
+function addStyleResource (rule) {
+  rule.use('style-resource')
+    .loader('style-resources-loader')
+    .options({
+      patterns: [
+        path.resolve(__dirname, './src/styles/imports.styl'),
+      ],
+    })
 }
