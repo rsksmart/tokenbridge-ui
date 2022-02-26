695d852df3cff946dccab164f6777ca71ee43fc7
aguschirico <aguschirico@gmail.com>
2022-02-26 13:48:36 +0100

    WIP: new slider with vuetify

--- src/components/rangeInput/RangeInput.vue
+++ src/components/rangeInput/RangeInput.vue
@@ -1,19 +1,7 @@
 <template>
-  <input
-    ref="rangeInput"
-    type="range"
-    :min="min"
-    :max="max"
-    :step="step"
-    :value="value"
-    :disabled="disabled"
-    list="input-range"
-    class="range-input"
-    @input="handleInputValue"
-  />
-  <datalist v-if="showMarks" id="input-range">
-    <option v-for="mark in marks" :key="mark" :value="mark" :label="`${mark}%`"></option>
-  </datalist>
+  <v-slider ref="rangeInput" v-model="value" :tick-labels="labels" :max="max" :min="min" thumb-color="green"
+            :step="step" :ticks="labels" show-ticks="always" tick-size="4" :disabled="disabled" track-fill-color="green" track-color="grey"
+            @change="handleInputValue"/>
 </template>
 
 <script>
@@ -49,6 +37,13 @@ export default {
   data() {
     return {
       marks: [],
+      labels: {
+        0: '0',
+        25: '25',
+        50: '50',
+        75: '75',
+        100: '100',
+      },
     }
   },
   mounted() {
@@ -58,11 +53,12 @@ export default {
         .fill(0)
         .map((value, index) => value + index * this.step)
     }
-    this.handleInputValue({ target: this.$refs.rangeInput })
+    // this.handleInputValue({ target: this.$refs.rangeInput })
   },
   methods: {
-    handleInputValue($event) {
-      const value = Number($event.target.value)
+    handleInputValue(value) {
+
+      console.log(value)
       $event.target.style.backgroundSize =
         ((value - this.min) * 100) / (this.max - this.min) + '% 100%'
       this.$emit('update:value', value)
@@ -72,18 +68,19 @@ export default {
 </script>
 
 <style scoped>
-.range-input {
+/* .range-input {
   -webkit-appearance: none;
   margin-right: 15px;
   width: 100%;
   height: 7px;
+  top: 50%;
   border-radius: 5px;
   background-size: 70% 100%;
   background: rgba(0, 0, 0, 0.1) linear-gradient(var(--primary), var(--primary)) no-repeat;
-}
+} */
 
 /* Input Thumb */
-.range-input::-webkit-slider-thumb {
+/* .v-slider-thumb {
   -webkit-appearance: none;
   height: 20px;
   width: 20px;
@@ -92,21 +89,25 @@ export default {
   cursor: ew-resize;
   box-shadow: 0 0 2px 0 #555;
   transition: background 0.3s ease-in-out;
-}
+} */
 
-.range-input::-webkit-slider-thumb:hover,
+/* .range-input::-webkit-slider-thumb:hover,
 .range-input::-moz-range-thumb:hover,
 .range-input::-ms-thumb:hover {
   background: var(--primary);
-}
+} */
 
 /* Input Track */
-.range-input::-webkit-slider-runnable-track,
+/* .range-input::-webkit-slider-runnable-track,
 .range-input::-moz-range-track,
 .range-input::-ms-track {
   -webkit-appearance: none;
   box-shadow: none;
   border: none;
   background: transparent;
-}
+} */
+</style>
+<style lang="scss" scoped>
+$slider-vertical-margin-top: 2vh;
+$slider-thumb-focused-size-increase: undefined !important;
 </style>
