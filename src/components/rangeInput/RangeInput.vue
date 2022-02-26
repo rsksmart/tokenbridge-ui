8bc5d5909e0c7d5a39aa74a830f0be0f166cf5a2
augusto.chirico <augusto.chirico@iovlabs.org>
2022-02-26 16:46:56 +0100

    working version. Cleanup and styling needed.

--- src/components/rangeInput/RangeInput.vue
+++ src/components/rangeInput/RangeInput.vue
@@ -1,7 +1,6 @@
 <template>
   <v-slider ref="rangeInput" v-model="value" :tick-labels="labels" :max="max" :min="min" thumb-color="green"
-            :step="step" :ticks="labels" show-ticks="always" tick-size="4" :disabled="disabled" track-fill-color="green" track-color="grey"
-            @change="handleInputValue"/>
+            :step="step" :ticks="labels" show-ticks="always" tick-size="4" :disabled="disabled" track-fill-color="green" track-color="grey"/>
 </template>
 
 <script>
@@ -46,6 +45,11 @@ export default {
       },
     }
   },
+  watch: {
+    value(newPercentage) {
+      this.$emit('update:value', newPercentage)
+    },    
+  },
   mounted() {
     if (this.showMarks) {
       const numberOfMarks = (this.max - this.min) / this.step
@@ -55,57 +59,9 @@ export default {
     }
     // this.handleInputValue({ target: this.$refs.rangeInput })
   },
-  methods: {
-    handleInputValue(value) {
-      console.log(value)
-      $event.target.style.backgroundSize =
-        ((value - this.min) * 100) / (this.max - this.min) + '% 100%'
-      this.$emit('update:value', value)
-    },
-  },
 }
 </script>
 
-<style scoped>
-/* .range-input {
-  -webkit-appearance: none;
-  margin-right: 15px;
-  width: 100%;
-  height: 7px;
-  top: 50%;
-  border-radius: 5px;
-  background-size: 70% 100%;
-  background: rgba(0, 0, 0, 0.1) linear-gradient(var(--primary), var(--primary)) no-repeat;
-} */
-
-/* Input Thumb */
-/* .v-slider-thumb {
-  -webkit-appearance: none;
-  height: 20px;
-  width: 20px;
-  border-radius: 50%;
-  background: var(--primary);
-  cursor: ew-resize;
-  box-shadow: 0 0 2px 0 #555;
-  transition: background 0.3s ease-in-out;
-} */
-
-/* .range-input::-webkit-slider-thumb:hover,
-.range-input::-moz-range-thumb:hover,
-.range-input::-ms-thumb:hover {
-  background: var(--primary);
-} */
-
-/* Input Track */
-/* .range-input::-webkit-slider-runnable-track,
-.range-input::-moz-range-track,
-.range-input::-ms-track {
-  -webkit-appearance: none;
-  box-shadow: none;
-  border: none;
-  background: transparent;
-} */
-</style>
 <style lang="scss" scoped>
 $slider-vertical-margin-top: 2vh;
 $slider-thumb-focused-size-increase: undefined !important;
