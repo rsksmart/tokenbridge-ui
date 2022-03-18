4d0f1e50cf45b75e246b313ca14a1fa1d75f6aec
Guilherme Soares <guilhermeluzsoares@gmail.com>
2022-03-18 17:11:36 -0300

    Removed duplicated method

--- src/constants/networks.js
+++ src/constants/networks.js
@@ -213,13 +213,6 @@ export function findNetworkByChainId(chainId, crossToNetworkId) {
   )
 }
 
-export function findNetworkById(id) {
-  const networks = getNetworksAvailable()
-  return networks.find(
-    net => net.networkId === id,
-  )
-}
-
 export function getNetworksConf(selectedChainId, prevChainId = null) {
   const networksAvailable = getNetworksAvailable()
   const networks = networksAvailable.filter(net => net.networkId === selectedChainId)
