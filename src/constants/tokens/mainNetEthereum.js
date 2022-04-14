7ee5e93b662f6a2d5cc39f4bb450690c7f943367
augusto.chirico <augusto.chirico@iovlabs.org>
2022-04-14 11:42:35 +0200

    removing all tokens except DAI, LINK, USDT & BUND

--- src/constants/tokens/mainNetEthereum.js
+++ src/constants/tokens/mainNetEthereum.js
@@ -12,23 +12,9 @@ export const MAIN_NET_ETHEREUM_GAS_TOKEN = {
 
 export const MAIN_NET_ETHEREUM_TOKENS = [
   {
-    ...tokensInfo.TOKEN_DOC_INFO,
-    symbol: 'eDOC',
-    address: '0x69f6d4D4813F8e2e618DAE7572e04b6D5329E207',
-    decimals: 18,
-    methodType: methodType.RECEIVER,
-  },
-  {
     ...tokensInfo.TOKEN_DAI_INFO,
     symbol: 'DAI',
     address: '0x6b175474e89094c44da98b954eedeac495271d0f',
-    decimals: 18,
-    methodType: methodType.RECEIVER,
-  },
-  {
-    ...tokensInfo.TOKEN_RIF_INFO,
-    symbol: 'eRIF',
-    address: '0x73c08467e23f7dcb7ddbbc8d05041b74467a498a',
     decimals: 18,
     methodType: methodType.RECEIVER,
   },
@@ -47,44 +33,9 @@ export const MAIN_NET_ETHEREUM_TOKENS = [
     methodType: methodType.RECEIVER,
   },
   {
-    ...tokensInfo.TOKEN_USDC_INFO,
-    symbol: 'USDC',
-    address: '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48',
-    decimals: 6,
-    methodType: methodType.RECEIVER,
-  },
-  {
-    ...tokensInfo.TOKEN_FLIXX_INFO,
-    symbol: 'FLIXX',
-    address: '0xf04a8ac553fcedb5ba99a64799155826c136b0be',
-    decimals: 18,
-    methodType: methodType.RECEIVER,
-  },
-  {
-    ...tokensInfo.TOKEN_RFOX_INFO,
-    symbol: 'RFOX',
-    address: '0xa1d6df714f91debf4e0802a542e13067f31b8262',
-    decimals: 18,
-    methodType: methodType.RECEIVER,
-  },
-  {
     ...tokensInfo.TOKEN_BUND_INFO,
     symbol: 'BUND',
     address: '0x8d3e855f3f55109d473735ab76f753218400fe96',
-    decimals: 18,
-    methodType: methodType.RECEIVER,
-  },
-  {
-    ...tokensInfo.TOKEN_AMLT_INFO,
-    symbol: 'AMLT',
-    address: '0xca0e7269600d353f70b14ad118a49575455c0f2f',
-    decimals: 18,
-    methodType: methodType.RECEIVER,
-  },
-  {
-    ...tokensInfo.TOKEN_UBI_INFO,
-    symbol: 'UBI',
-    address: '0xdd1ad9a21ce722c151a836373babe42c868ce9a4',
     decimals: 18,
     methodType: methodType.RECEIVER,
   },
--- src/constants/tokensInfo.js
+++ src/constants/tokensInfo.js
@@ -1,10 +1,4 @@
 // --------- TOKENS export --------------
-export const TOKEN_DOC_INFO = {
-  token: 'DOC',
-  name: 'Dollar on Chain',
-  typeId: 4,
-  icon: 'https://raw.githubusercontent.com/rsksmart/rsk-contract-metadata/master/images/doc.png',
-}
 
 export const TOKEN_RBTC_INFO = {
   token: 'RBTC',
@@ -20,49 +14,11 @@ export const TOKEN_DAI_INFO = {
   icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x6B175474E89094C44Da98b954EedeAC495271d0F/logo.png',
 }
 
-export const TOKEN_SAI_INFO = {
-  token: 'SAI',
-  name: 'Sai Stablecoin',
-  typeId: 4,
-  icon: 'https://raw.githubusercontent.com/rsksmart/rsk-testnet-contract-metadata/master/images/sai.png',
-}
-
-// export const INV_TOKEN = { token: 'INV', name: 'Invest', icon: 'https://raw.githubusercontent.com/rsksmart/rsk-contract-metadata/master/images/inv.png',
-// };
-
-export const TOKEN_BITPRO_INFO = {
-  token: 'BITPRO',
-  name: 'BitPro',
-  typeId: 0,
-  icon: 'https://raw.githubusercontent.com/rsksmart/rsk-contract-metadata/master/images/bpro.png',
-}
-
-export const TOKEN_RIF_INFO = {
-  token: 'RIF',
-  name: 'RIF',
-  typeId: 5,
-  icon: 'https://raw.githubusercontent.com/rsksmart/rsk-contract-metadata/master/images/rif.png',
-}
-
-export const TOKEN_TUSD_INFO = {
-  token: 'TUSD',
-  name: 'True USD',
-  typeId: 4,
-  icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x0000000000085d4780B73119b644AE5ecd22b376/logo.png',
-}
-
 export const TOKEN_WETH_INFO = {
   token: 'WETH',
   name: 'Wrapped Ether',
   typeId: 1,
   icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2/logo.png',
-}
-
-export const TOKEN_ZRX_INFO = {
-  token: 'ZRX',
-  name: '0x Protocol',
-  typeId: 5,
-  icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xE41d2489571d322189246DaFA5ebDe1F4699F498/logo.png',
 }
 
 export const TOKEN_LINK_INFO = {
@@ -79,58 +35,9 @@ export const TOKEN_USDT_INFO = {
   icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xdAC17F958D2ee523a2206206994597C13D831ec7/logo.png',
 }
 
-export const TOKEN_USDC_INFO = {
-  token: 'USDC',
-  name: 'USD Coin',
-  typeId: 4,
-  icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48/logo.png',
-}
-
-export const TOKEN_FLIXX_INFO = {
-  token: 'FLIXX',
-  name: 'Flixxo',
-  typeId: 6,
-  icon: 'https://raw.githubusercontent.com/rsksmart/rsk-contract-metadata/master/images/flixx.png',
-}
-
-export const TOKEN_RFOX_INFO = {
-  token: 'RFOX',
-  name: 'RedFOX Labs',
-  typeId: 6,
-  icon: 'https://raw.githubusercontent.com/rsksmart/rsk-contract-metadata/master/images/rfox.png',
-}
-
-export const TOKEN_OLD_INFO = {
-  token: 'Ω',
-  name: 'Ω Old Mintable',
-  typeId: 6,
-  icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0xE41d2489571d322189246DaFA5ebDe1F4699F498/logo.png',
-}
-
 export const TOKEN_BUND_INFO = {
   token: 'BUND',
   name: 'Bundles Finance',
   typeId: 3,
   icon: 'https://raw.githubusercontent.com/rsksmart/rsk-contract-metadata/master/images/bund.png',
-}
-
-export const TOKEN_AMLT_INFO = {
-  token: 'AMLT',
-  name: 'AMLT Coinfirm',
-  typeId: 6,
-  icon: 'https://raw.githubusercontent.com/rsksmart/rsk-contract-metadata/master/images/amlt.png',
-}
-
-export const TOKEN_UBI_INFO = {
-  token: 'UBI',
-  name: 'Universal Basic Income',
-  typeId: 6,
-  icon: 'https://raw.githubusercontent.com/rsksmart/rsk-contract-metadata/master/images/ubi.png',
-}
-
-export const TOKEN_WBTC_INFO = {
-  token: 'WBTC',
-  name: 'Wrapped BTC',
-  typeId: 0,
-  icon: 'https://raw.githubusercontent.com/trustwallet/assets/master/blockchains/ethereum/assets/0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599/logo.png',
 }
