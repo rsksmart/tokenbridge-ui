6deae4075c44da0fad9cdd9cef0e22c6d0caeaee
augusto.chirico <augusto.chirico@iovlabs.org>
2022-04-29 19:31:51 +0200

    use mock web3 provider

--- cypress/integration/1-home/home.spec.js
+++ cypress/integration/1-home/home.spec.js
@@ -1,8 +1,9 @@
 // https://docs.cypress.io/api/introduction/api.html
+import { CONNECT_WALLET_BUTTON } from '../../pages/home'
 
 describe('App is alive', () => {
   it('Connect wallet button should be present', async () => {
     cy.visit('/')
-    cy.get('#logIn').contains('Connect wallet')
+    cy.get(CONNECT_WALLET_BUTTON).contains('Connect wallet')
   })
 })
